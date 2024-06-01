00a59c846d1099aa45145ce47e9961c3573a1f48
Taha Hussein <taha.hussein.two@gmail.com>
2024-06-01 16:14:48 +0300

    Initial Commit

---
+++ Elm/ElmApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElmApp.swift
+//  Elm
+//
+//  Created by Taha Hussein on 01/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ElmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
